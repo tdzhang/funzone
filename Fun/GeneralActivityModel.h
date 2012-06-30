34f99369c764dfa686c996a1d595f880a76d2404
Tongda Zhang <tongda.zhang@gmail.com>
2012-06-30 15:57:02 -0700

    added tdzhangs changes

---
+++ Fun/GeneralActivityModel.h
@@ -0,0 +1,15 @@
+//
+//  GeneralActivityModel.h
+//  SimpleChoosePage
+//
+//  Created by Tongda Zhang on 6/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+//the model for a simple user generated Activity
+
+#import <Foundation/Foundation.h>
+
+@interface GeneralActivityModel : NSObject
+@property (nonatomic,strong) NSString *activityCategory;
+@end
---
+++ Fun/UserContactObject.m
@@ -0,0 +1,16 @@
+//
+//  UserContactObject.m
+//  SimpleChoosePage
+//
+//  Created by Tongda Zhang on 6/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserContactObject.h"
+
+@implementation UserContactObject
+@synthesize firstName=_firstName;
+@synthesize lastName=_lastName;
+@synthesize email=_email;
+@synthesize phone=_phone;
+@end
