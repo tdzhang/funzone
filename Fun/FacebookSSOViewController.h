e7b107f592bfd3b0aa26b4e89ba5d01bf5c1d11e
Yizhou Zhu <antonyzyz@gmail.com>
2012-07-08 00:22:06 -0700

    Add FacebookSSOViewController

---
+++ Fun/FacebookSSOViewController.h
@@ -0,0 +1,13 @@
+//
+//  FacebookSSOViewController.h
+//  Fun
+//
+//  Created by Yizhou Zhu on 7/7/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacebookSSOViewController : UIViewController
+
+@end
---
+++ Fun/FacebookSSOViewController.m
@@ -0,0 +1,43 @@
+//
+//  FacebookSSOViewController.m
+//  Fun
+//
+//  Created by Yizhou Zhu on 7/7/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "FacebookSSOViewController.h"
+
+@interface FacebookSSOViewController ()
+
+@end
+
+@implementation FacebookSSOViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
