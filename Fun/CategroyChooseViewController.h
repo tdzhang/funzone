a3bc3a8ccdbba1e2cc3179bc0f6f2941ba7928be
Tongda Zhang <tongda.zhang@gmail.com>
2012-07-07 21:31:43 -0700

    add the self define category connection

--- Fun/CategroyChooseViewController.h
+++ Fun/CategroyChooseViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CategroyChooseViewController : UIViewController
+@interface CategroyChooseViewController : UIViewController<UIAlertViewDelegate>
 
 @end
--- Fun/CategroyChooseViewController.m
+++ Fun/CategroyChooseViewController.m
@@ -14,6 +14,7 @@
 #define GOTO_MOVIE_VIEWCONTROLLER_SNAPSHOT @"AddEventControllerSnapShot.png"
 #define GOTO_SHOPPING_VIEWCONTROLLER_SNAPSHOT @"AddEventControllerSnapShot.png"
 #define GOTO_PARTY_VIEWCONTROLLER_SNAPSHOT @"AddEventControllerSnapShot.png"
+#define GOTO_SELFDEFINE_VIEWCONTROLLER_SNAPSHOT @"AddEventControllerSnapShot.png"
 
 #import "CategroyChooseViewController.h"
 
@@ -21,12 +22,14 @@
 @property (nonatomic,strong) UIView *flash;
 @property (nonatomic,strong) UIImageView *flashBackImageView;
 @property (nonatomic,strong) NSString *eventPrepareCategory;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldSelfDefine;
 @end
 
 @implementation CategroyChooseViewController
 @synthesize flash=_flash;
 @synthesize flashBackImageView=_flashBackImageView;
 @synthesize eventPrepareCategory=_eventPrepareCategory;
+@synthesize textFieldSelfDefine = _textFieldSelfDefine;
 
 
 #pragma mark - Controller Life circle
@@ -59,6 +62,7 @@
 
 - (void)viewDidUnload
 {
+    [self setTextFieldSelfDefine:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -165,6 +169,83 @@
     [self FlashTransition1:GOTO_PARTY_VIEWCONTROLLER_SNAPSHOT];
     [self performSelector:@selector(GoToNextViewEvent:) withObject:sender afterDelay:FLASH_TRANSITION_DURATION*2];
 }
+//hit the self define button "Go"
+- (IBAction)SelfDefineButtonClicked:(id)sender {
+    UITextField *textField=self.textFieldSelfDefine;
+    //if input is empty, pop out the alert
+    if ([textField.text length]==0) {
+        UIAlertView *inputEmptyError = [[UIAlertView alloc] initWithTitle:@"Self Define Input Empty" message:@"You did not input anything" delegate:self  cancelButtonTitle:@"Input Again" otherButtonTitles:@"Cancel",nil];
+        inputEmptyError.delegate=self;
+        [inputEmptyError show];
+    }
+    //otherwise, do the segue
+    [self.textFieldSelfDefine resignFirstResponder];
+    [self FlashTransition1:GOTO_SELFDEFINE_VIEWCONTROLLER_SNAPSHOT];
+    [self performSelector:@selector(GoToNextViewEvent:) withObject:sender afterDelay:FLASH_TRANSITION_DURATION*2];
+}
 
+#pragma mark - testfield delegate method
+////////////////////////////////////////////////
+//implement the method for dealing with the textfield
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    if ([textField isEqual:self.textFieldSelfDefine]) {
+        [textField resignFirstResponder];
+        return YES;
+    }
+    return YES;
+}
+
+//the next 3 method deal with the keyboard covering with the text field
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    
+     //if edit the add cost textfield, the whole view need to 
+     //scroll up, get rid of the keyboard covering
+     if ([textField isEqual:self.textFieldSelfDefine]) {
+     [self animateTextField: textField up: YES];
+     }
+     
+}
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    
+     //if finished editign the add cost textfield, the whole view need to scroll down
+     if ([textField isEqual:self.textFieldSelfDefine]) {
+     [self animateTextField: textField up: NO];
+     }
+     
+}
+- (void) animateTextField: (UITextField*) textField up: (BOOL) up
+{
+    const int movementDistance = 40; // tweak as needed
+    const float movementDuration = 0.3f; // tweak as needed
+    
+    int movement = (up ? -movementDistance : movementDistance);
+    
+    [UIView beginAnimations: @"anim" context: nil];
+    [UIView setAnimationBeginsFromCurrentState: YES];
+    [UIView setAnimationDuration: movementDuration];
+    self.view.frame = CGRectOffset(self.view.frame, 0, movement);
+    [UIView commitAnimations];
+}
+
+#pragma mark - deal with alert view
+////////////////////////////////////////////////
+//implement the method for dealing with the return of the alertView
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    //NSLog(@"%@",alertView.title);
+    //deal with the Input Empty Error for the activity category choose
+    if ([alertView.title isEqualToString:@"Self Define Input Empty"]) {
+        //NSLog(@"%d",buttonIndex);
+        if (buttonIndex == 0) {
+            //Input Again
+            [self.textFieldSelfDefine becomeFirstResponder];
+        }
+        else if(buttonIndex == 1){
+            //Cancel
+            [self.textFieldSelfDefine resignFirstResponder];
+        }
+    }
+}
 
 @end
