78fce5b5ff4ca3532c3da95943386a186a87eb5d
Yizhou Zhu <antonyzyz@gmail.com>
2012-07-10 12:30:01 -0700

    add Json framework

---
+++ Fun/Movies.h
@@ -0,0 +1,13 @@
+//
+//  Movies.h
+//  Fun
+//
+//  Created by Yizhou Zhu on 7/9/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Movies : NSObject
+
+@end
---
+++ Fun/Movies.m
@@ -0,0 +1,13 @@
+//
+//  Movies.m
+//  Fun
+//
+//  Created by Yizhou Zhu on 7/9/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "Movies.h"
+
+@implementation Movies
+
+@end
