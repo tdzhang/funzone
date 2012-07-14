a3da71ae4c8a34451a4ecd8ed8553da4cc982da5
Tongda Zhang <tongda.zhang@gmail.com>
2012-07-14 16:22:49 -0700

    add detail part, need further recise

--- Fun/DetailViewController.m
+++ Fun/DetailViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "DetailViewController.h"
+#import "Cache.h"
 
 @interface DetailViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *contributorProfileImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *eventImageView;
 @property (weak, nonatomic) IBOutlet UILabel *contributorNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *timestampLabel;
 @property (weak, nonatomic) IBOutlet UILabel *eventTitleLabel;
@@ -19,11 +21,13 @@
 @property (weak, nonatomic) IBOutlet UILabel *eventPriceLabel;
 @property (weak, nonatomic) IBOutlet UILabel *eventIntroLabel;
 @property (weak, nonatomic) IBOutlet UIScrollView *myScrollView;
+@property (nonatomic,strong) NSMutableData *data;
 
 @end
 
 @implementation DetailViewController
 @synthesize contributorProfileImageView;
+@synthesize eventImageView;
 @synthesize contributorNameLabel;
 @synthesize timestampLabel;
 @synthesize eventTitleLabel;
@@ -33,6 +37,7 @@
 @synthesize eventPriceLabel;
 @synthesize eventIntroLabel;
 @synthesize myScrollView;
+@synthesize data=_data;
 
 #pragma mark - View Life Circle
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -62,6 +67,7 @@
     [self setEventPriceLabel:nil];
     [self setEventIntroLabel:nil];
     [self setMyScrollView:nil];
+    [self setEventImageView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -69,12 +75,112 @@
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     
-    [self.myScrollView setContentSize:CGSizeMake(320, 1000)];
+    [self.myScrollView setContentSize:CGSizeMake(320, 500)];
+    
+    //start a new connection, to fetch data from the server (about event detail)
+    NSString *request_string=[NSString stringWithFormat:@"http://www.funnect.me/events/view?event_id=3&shared_event_id=1"];
+    NSLog(@"%@",request_string);
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:request_string]];
+    NSURLConnection *connection=[[NSURLConnection alloc] initWithRequest:request delegate:self];
+    [connection start];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - implement NSURLconnection delegate methods 
+//to deal with the returned data
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    self.data = [[NSMutableData alloc] init];
+}
+
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [self.data appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    /*UIAlertView *someError = [[UIAlertView alloc] initWithTitle:@"Connection Error" message: @"Unable to connect to searching server" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+     [someError show];*/
+    //NSLog(@"%@",connection.originalRequest.URL);
+    //NSLog(@"%@",error);
+}
+
+
+//when the connection get the returned data (json form)
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    //renew the 10 newest features!!!!
+    NSError *error;
+    NSDictionary *event = [NSJSONSerialization JSONObjectWithData:self.data options:kNilOptions error:&error];
+    //after reget the newest 10 popular event, the next page that need to be retrait is page 2
+
+
+        NSString *title=[event objectForKey:@"title"];
+        NSString *description=[event objectForKey:@"description"];
+        NSString *photo=[event objectForKey:@"photo_url"];
+        NSString *time=[event objectForKey:@"time"];
+       // NSString *num_pins=[NSString stringWithFormat:@"%@",[event objectForKey:@"num_pins"]];
+        //NSString *num_views=[NSString stringWithFormat:@"%@",[event objectForKey:@"num_views"]];
+        NSString *locationName=[event objectForKey:@"location"];
+       // NSString *longitude=[NSString stringWithFormat:@"%f",[event objectForKey:@"longitude"]];
+       // NSString *latitude=[NSString stringWithFormat:@"%f",[event objectForKey:@"latitude"]];
+
+        if (!title) {
+            return;
+        }
+        
+        [self.eventTitleLabel setText:title];
+        [self.eventLocationLabel setText:locationName];
+        [self.eventTimeLabel setText:time];
+        [self.eventIntroLabel setText:description];
+        
+        
+        //NSLog(@"%@",title);
+        //NSLog(@"%@",photo);
+        //NSLog(@"%@",description);
+        //NSLog(@"%@",num_pins);
+        //NSLog(@"%@",num_views);
+        
+        NSURL *url=[NSURL URLWithString:photo];
+        if (![Cache isURLCached:url]) {
+            //using high priority queue to fetch the image
+            dispatch_async( dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH,0),^{  
+                //get the image data
+                NSData * imageData = nil;
+                imageData = [[NSData alloc] initWithContentsOfURL: url];
+                
+                if ( imageData == nil ){
+                    //if the image data is nil, the image url is not reachable. using a default image to replace that
+                    //NSLog(@"downloaded %@ error, using a default image",url);
+                    UIImage *image=[UIImage imageNamed:@"monterey.jpg"];
+                    imageData=UIImagePNGRepresentation(image);
+                    if(imageData){
+                        dispatch_async( dispatch_get_main_queue(),^{
+                            [Cache addDataToCache:url withData:imageData];
+                            [self.eventImageView setImage:image];
+                        });
+                    }
+                }
+                else {
+                    //else, the image date getting finished, directlhy put it in the cache, and then reload the table view data.
+                    //NSLog(@"downloaded %@",url);
+                    if(imageData){
+                        dispatch_async( dispatch_get_main_queue(),^{
+                            [Cache addDataToCache:url withData:imageData];
+                            [self.eventImageView setImage:[UIImage imageWithData:imageData]];
+                        });
+                    }
+                }
+            });
+        }
+        else {
+            dispatch_async( dispatch_get_main_queue(),^{
+                [self.eventImageView setImage:[UIImage imageWithData:[Cache getCachedData:url]]];
+            });
+        }
 }
 
 @end
