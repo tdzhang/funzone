30762950e006cada089500c696b4a336ee9a3b38
Tongda Zhang <tongda.zhang@gmail.com>
2012-07-16 08:53:36 -0700

    add the down fretting part

--- Fun/ExploreViewController.m
+++ Fun/ExploreViewController.m
@@ -17,6 +17,7 @@
 @property (nonatomic,retain) DetailViewController *detailViewController;
 @property (nonatomic,retain) NSMutableArray *blockViews;
 @property (nonatomic,retain) UIImageView *refreshView;
+@property (nonatomic,retain) UIImageView *refreshViewdown;
 @property (nonatomic,strong) NSMutableData *data;
 @property (nonatomic,strong) NSString *freshConnectionType;
 @property (nonatomic) int refresh_page_num;
@@ -26,6 +27,7 @@
 
 @implementation ExploreViewController
 @synthesize refreshView=_refreshView;
+@synthesize refreshViewdown=_refreshViewdown;
 @synthesize detailViewController = _detailViewController;
 @synthesize blockViews = _blockViews;
 @synthesize currentY = _currentY;
@@ -130,7 +132,7 @@
     }
     
     //this is the upper most position that need to reget the most popular 10 events
-    if (scrollView.contentOffset.y<-BlOCK_VIEW_HEIGHT/2) {
+    if (scrollView.contentOffset.y<-BlOCK_VIEW_HEIGHT/3) {
         //remove the main views
         for (UIView *view in [self.mainScrollView subviews]) {
             [view setFrame:CGRectMake(0, view.frame.origin.y+BlOCK_VIEW_HEIGHT, view.frame.size.width, view.frame.size.height)];
@@ -172,9 +174,29 @@
         [connection start];
     }
     //add more
-    else if(scrollView.contentOffset.y>BlOCK_VIEW_HEIGHT*(([self.blockViews count]-5))){
+    else if(scrollView.contentOffset.y>BlOCK_VIEW_HEIGHT*(([self.blockViews count]-2.5))){
+        //add the content add refresh indicator
+        for(UIView *subview in [self.refreshViewdown subviews]) {
+            [subview removeFromSuperview];
+        }
+        
+        UIView*loading =[[UIView alloc] initWithFrame:CGRectMake(0,BlOCK_VIEW_HEIGHT*([self.blockViews count]+1),VIEW_WIDTH,BlOCK_VIEW_HEIGHT)];
+        loading.layer.cornerRadius =15;
+        loading.opaque = NO;
+        loading.backgroundColor =[UIColor colorWithWhite:0.0f alpha:0.3f];
+        UILabel*loadLabel =[[UILabel alloc] initWithFrame:CGRectMake(120,25,80,40)];
+        loadLabel.text =@"Adding More";loadLabel.font =[UIFont boldSystemFontOfSize:18.0f];
+        loadLabel.textAlignment =UITextAlignmentCenter;
+        loadLabel.textColor =[UIColor colorWithWhite:1.0f alpha:1.0f];
+        loadLabel.backgroundColor =[UIColor clearColor];
+        [loading addSubview:loadLabel];
+        UIActivityIndicatorView*spinning =[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+        spinning.frame =CGRectMake(120,80,80,80);
+        [spinning startAnimating];[loading addSubview:spinning];
+        [self.refreshView addSubview:loading];
+        self.mainScrollView.contentSize =CGSizeMake(VIEW_WIDTH, ([self.blockViews count]+1)*BlOCK_VIEW_HEIGHT);
+        
         //NSLog(@"add more");
-
         NSString *request_string=[NSString stringWithFormat:@"http://www.funnect.me/events/featured?page=%d",self.refresh_page_num];
         NSLog(@"%@",request_string);
         NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:request_string]];
