25ede32a5b5413901a9d98bb5520b035b02e0acd
Tongda Zhang <tongda.zhang@gmail.com>
2012-07-29 15:41:51 -0700

    change the log out// wait for the setting page to be finished

--- Fun/MoreTableViewController.m
+++ Fun/MoreTableViewController.m
@@ -90,58 +90,67 @@
 #pragma mark - Table view delegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    //notification
-    if (indexPath.row == 0) {
+    if (indexPath.section == 0) {
+        ;
+    }
+    else if (indexPath.section == 1){
         
     }
-    //log out
-    else if(indexPath.row == 1){
-        //logout from facebook
-        FunAppDelegate *funAppdelegate=[[UIApplication sharedApplication] delegate];
-        if (!funAppdelegate.facebook) {
-            funAppdelegate.facebook = [[Facebook alloc] initWithAppId:FACEBOOK_APP_ID andDelegate:(id)funAppdelegate];
-        }
-        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-        if ([defaults objectForKey:@"FBAccessTokenKey"] && [defaults objectForKey:@"FBExpirationDateKey"]) {
-            funAppdelegate.facebook.accessToken = [defaults objectForKey:@"FBAccessTokenKey"];
-            NSLog(@"%@",funAppdelegate.facebook.accessToken);
-            funAppdelegate.facebook.expirationDate = [defaults objectForKey:@"FBExpirationDateKey"];
-            [funAppdelegate.facebook logout:(id)funAppdelegate];
-        }
-        
-        //signout the auth_token
-        NSURL *url=[NSURL URLWithString:[NSString stringWithFormat:@"%@/users/sign_out.json?auth_token=%@",CONNECT_DOMIAN_NAME,[defaults objectForKey:@"login_auth_token"]]];
-        ASIFormDataRequest *request = [ASIFormDataRequest requestWithURL:url];
-        __block ASIFormDataRequest *block_request=request;
-        [request setCompletionBlock:^{
-            // Use when fetching text data
-            NSString *responseString = [block_request responseString];
-            NSLog(@"%@",responseString);
+    else if (indexPath.section ==2){
+        //notification
+        if (indexPath.row == 0) {
             
-            NSError *error;
-            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:block_request.responseData options:kNilOptions error:&error];
-            if ([[json objectForKey:@"response"] isEqualToString:@"ok"]) {
-                UIAlertView *success = [[UIAlertView alloc] initWithTitle:@"Log out complete!" message:@"You have successfully logged out." delegate:self  cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
-                success.delegate=self;
-                [success show];
+        }
+        //log out
+        else if(indexPath.row == 1){
+            //logout from facebook
+            FunAppDelegate *funAppdelegate=[[UIApplication sharedApplication] delegate];
+            if (!funAppdelegate.facebook) {
+                funAppdelegate.facebook = [[Facebook alloc] initWithAppId:FACEBOOK_APP_ID andDelegate:(id)funAppdelegate];
             }
-        }];
-        [request setFailedBlock:^{
-            NSError *error = [block_request error];
-            NSLog(@"%@",error.description);
-            UIAlertView *notsuccess = [[UIAlertView alloc] initWithTitle:@"Log Out Error!" message: [NSString stringWithFormat:@"Error: %@",error.description ] delegate:self  cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
-            notsuccess.delegate=self;
-            [notsuccess show];
-        }];
-        
-        //add login auth_token //add content
-        //[request setPostValue:[defaults objectForKey:@"login_auth_token"] forKey:@"auth_token"];
-        [request setRequestMethod:@"DELETE"];
-        [request startAsynchronous];
-        
-        [defaults setValue:nil forKey:@"login_auth_token"];
-        [defaults synchronize];
+            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+            if ([defaults objectForKey:@"FBAccessTokenKey"] && [defaults objectForKey:@"FBExpirationDateKey"]) {
+                funAppdelegate.facebook.accessToken = [defaults objectForKey:@"FBAccessTokenKey"];
+                NSLog(@"%@",funAppdelegate.facebook.accessToken);
+                funAppdelegate.facebook.expirationDate = [defaults objectForKey:@"FBExpirationDateKey"];
+                [funAppdelegate.facebook logout:(id)funAppdelegate];
+            }
+            
+            //signout the auth_token
+            NSURL *url=[NSURL URLWithString:[NSString stringWithFormat:@"%@/users/sign_out.json?auth_token=%@",CONNECT_DOMIAN_NAME,[defaults objectForKey:@"login_auth_token"]]];
+            ASIFormDataRequest *request = [ASIFormDataRequest requestWithURL:url];
+            __block ASIFormDataRequest *block_request=request;
+            [request setCompletionBlock:^{
+                // Use when fetching text data
+                NSString *responseString = [block_request responseString];
+                NSLog(@"%@",responseString);
+                
+                NSError *error;
+                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:block_request.responseData options:kNilOptions error:&error];
+                if ([[json objectForKey:@"response"] isEqualToString:@"ok"]) {
+                    UIAlertView *success = [[UIAlertView alloc] initWithTitle:@"Log out complete!" message:@"You have successfully logged out." delegate:self  cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
+                    success.delegate=self;
+                    [success show];
+                }
+            }];
+            [request setFailedBlock:^{
+                NSError *error = [block_request error];
+                NSLog(@"%@",error.description);
+                UIAlertView *notsuccess = [[UIAlertView alloc] initWithTitle:@"Log Out Error!" message: [NSString stringWithFormat:@"Error: %@",error.description ] delegate:self  cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
+                notsuccess.delegate=self;
+                [notsuccess show];
+            }];
+            
+            //add login auth_token //add content
+            //[request setPostValue:[defaults objectForKey:@"login_auth_token"] forKey:@"auth_token"];
+            [request setRequestMethod:@"DELETE"];
+            [request startAsynchronous];
+            
+            [defaults setValue:nil forKey:@"login_auth_token"];
+            [defaults synchronize];
+        }
     }
+    
 }
 /*
     NSString* urlString=[self.imageUrls objectAtIndex:indexPath.row];
