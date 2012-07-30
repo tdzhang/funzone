d3663c2978d32a98a44042833944c8b878d9e6c9
Tongda Zhang <tongda.zhang@gmail.com>
2012-07-30 15:40:06 -0700

    add search top page

---
+++ Fun/FIndTopTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FIndTopTableViewController.h
+//  Fun
+//
+//  Created by Tongda Zhang on 7/30/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FIndTopTableViewController : UITableViewController
+
+@end
