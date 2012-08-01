e05ba261d6530d82b145382121adc251ce2e72ea
Tongda Zhang <tongda.zhang@gmail.com>
2012-07-31 23:24:31 -0700

    add activity object

---
+++ Fun/activityElementObject.h
@@ -0,0 +1,13 @@
+//
+//  activityElementObject.h
+//  OrangeParc
+//
+//  Created by Tongda Zhang on 7/31/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface activityElementObject : NSObject
+
+@end
---
+++ Fun/activityElementObject.m
@@ -0,0 +1,13 @@
+//
+//  activityElementObject.m
+//  OrangeParc
+//
+//  Created by Tongda Zhang on 7/31/12.
+//
+//
+
+#import "activityElementObject.h"
+
+@implementation activityElementObject
+
+@end
