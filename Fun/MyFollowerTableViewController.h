f5a98a7facb0170ff6a0e9a8a32ba9f09d6058d2
Tongda Zhang <tongda.zhang@gmail.com>
2012-08-06 17:43:45 -0700

    add myfollower

---
+++ Fun/MyFollowerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyFollowerTableViewController.h
+//  OrangeParc
+//
+//  Created by Tongda Zhang on 8/6/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyFollowerTableViewController : UITableViewController
+
+@end
