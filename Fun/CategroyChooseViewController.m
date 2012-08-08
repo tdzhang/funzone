4574a866e059daa1a8573be1013d8edea0372289
Mary Yang <yanghemary@gmail.com>
2012-08-08 00:13:40 -0700

    back button

--- Fun/CategroyChooseViewController.m
+++ Fun/CategroyChooseViewController.m
@@ -70,6 +70,12 @@
     //Navigation Bar Style
     [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"header.png"] forBarMetrics:UIBarMetricsDefault];
     self.navigationController.navigationBarHidden = NO;
+    
+    UIBarButtonItem *backButton = [[UIBarButtonItem alloc]
+                                   initWithTitle:@"Back" style:UIBarButtonItemStyleBordered
+                                   target:nil action:nil];
+    backButton.tintColor = [UIColor colorWithRed:0.94111 green:0.6373 blue:0.3 alpha:1];
+    [self.navigationItem setBackBarButtonItem:backButton];
         
 }
 
