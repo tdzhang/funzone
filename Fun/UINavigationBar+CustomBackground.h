b9a8359a93e1383d0b6f10945930986dea1f7a1c
Mary Yang <yanghemary@gmail.com>
2012-08-07 19:31:18 -0700

    null

---
+++ Fun/UINavigationBar+CustomBackground.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationBar+CustomBackground.h
+//  OrangeParc
+//
+//  Created by He Yang on 8/7/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationBar (CustomBackground)
+
+@end
---
+++ Fun/UINavigationBar+CustomBackground.m
@@ -0,0 +1,13 @@
+//
+//  UINavigationBar+CustomBackground.m
+//  OrangeParc
+//
+//  Created by He Yang on 8/7/12.
+//
+//
+
+#import "UINavigationBar+CustomBackground.h"
+
+@implementation UINavigationBar (CustomBackground)
+
+@end
