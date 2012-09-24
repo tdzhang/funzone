2e33e7ac120ec18a4603f43b0f8ebdadc266ddf1
Tongda Zhang <tongda.zhang@gmail.com>
2012-09-23 22:04:11 -0700

    activity crash bug fixed

--- Fun/ActivityTableViewCell.m
+++ Fun/ActivityTableViewCell.m
@@ -134,7 +134,12 @@
 //        element.event_pic=DEFAULT_IMAGE_PLACEHOLDER;
 //    }
     NSLog(@"-->%@",element.event_pic);
+    NSLog(@"%@",element.event_pic);
+    if (!element.event_pic||[[NSString stringWithFormat:@"%@",element.event_pic] isEqualToString:@"<null>"]) {
+        element.event_pic = DEFAULT_IMAGE_PLACEHOLDER;
+    }
     NSURL *eventUrl=[NSURL URLWithString:element.event_pic];
+
     //deal with the profile image
     if (![Cache isURLCached:eventUrl]) {
         //using high priority queue to fetch the image
